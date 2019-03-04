474ad04edebc5f78c71cd3c6e7926ae5fb9f36f5
Hanna Vidzianeyeva <Hanna_Vidzianeyeva@epam.com>
2019-03-04 12:31:49 +0300

    Added the Owin Startup class and the WebApi Configuration class

---
+++ LanguageMentor.Web.Api/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using Owin;
+using LanguageMentor.Web.Api.App_Start;
+
+namespace LanguageMentor.Web.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Configure(config);
+            app.UseWebApi(config);
+        }
+    }
+}
