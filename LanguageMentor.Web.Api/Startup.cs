ee179a20b32b627e6fb9ed60896ced2511676ace
Hanna Vidzianeyeva <Hanna_Vidzianeyeva@epam.com>
2019-03-11 11:41:07 +0300

    Added OwinStartup

--- LanguageMentor.Web.Api/Startup.cs
+++ LanguageMentor.Web.Api/Startup.cs
@@ -1,9 +1,12 @@
 ﻿using System.Web.Http;
 using Owin;
+using Microsoft.Owin;
 using LanguageMentor.Core.IoC;
 using LanguageMentor.Core.IoC.Extensions;
+using LanguageMentor.Web.Api;
 using LanguageMentor.Web.Api.Configurations;
 
+[assembly: OwinStartup(typeof(Startup))]
 namespace LanguageMentor.Web.Api
 {
     public class Startup
