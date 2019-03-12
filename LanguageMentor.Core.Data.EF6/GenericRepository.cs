5a0f5b058058402ae5a3a204ad0a332c7159a177
Hanna Vidzianeyeva <Hanna_Vidzianeyeva@epam.com>
2019-03-12 13:20:43 +0300

    Added Data core projects

---
+++ LanguageMentor.Core.Data.EF6/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LanguageMentor.Core.Data.EF6
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public TEntity Find(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public IEnumerable<TEntity> Search(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate).ToList();
+        }
+    }
+}
---
+++ LanguageMentor.Core.Data/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace LanguageMentor.Core.Data
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Find(int id);
+
+        void Update(TEntity entity);
+
+        IEnumerable<TEntity> Search(Expression<Func<TEntity, bool>> predicate);
+    }
+}
